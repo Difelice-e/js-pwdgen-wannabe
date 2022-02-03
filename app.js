7535a99f403aad1d8bdae56c21585aacd2aead07
Difelice-e <difelice.emanuele@gmail.com>
2022-02-03 15:28:12 +0100

    pulizia codice

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-let userName = prompt("inserisci il tuo nome")
-console.log(userName)
+let userName = prompt("inserisci il tuo nome");
+// console.log(userName)
 
-let userSurname = prompt("Inserisci il tuo cognome")
-console.log(userSurname)
+let userSurname = prompt("Inserisci il tuo cognome");
+// console.log(userSurname)
 
-let userColor = prompt("Inserisci il tuo colore preferito")
-console.log(userColor)
+let userColor = prompt("Inserisci il tuo colore preferito");
+// console.log(userColor)
 
-let userPass = (userName) + (userSurname) + (userColor) + "21"
-console.log(userPass)
+let userPass = (userName) + (userSurname) + (userColor) + "21";
+// console.log(userPass)
 
-document.getElementById("password").innerHTML += (userPass)
+document.getElementById("password").innerHTML += (userPass);
